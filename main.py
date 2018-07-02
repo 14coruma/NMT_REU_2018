44068f2c199c62baaac6491458fd0f40d11de171
jenkinsdonovan <donovan.jenkins@student.nmt.edu>
2018-07-02 12:10:27 -0600

    lotta stuff

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
 import preprocess as pproc
+import sys
 import features as ft
 import trainer as tr
 import ui
 
 def main():
-    images, targets = pproc.process()
-
+    images, targets = pproc.process(sys.argv[1:])
+    """
     data = ft.features(images)
 
     tr.train(data, targets)
-
+    """
 if __name__ == "__main__":
     main()
