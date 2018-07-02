8bf81be5acdb4447885b96967a36e8160de2389d
jenkinsdonovan <donovan.jenkins@student.nmt.edu>
2018-07-02 16:17:51 -0600

    cleanup

--- preprocess.py
+++ preprocess.py
@@ -1,49 +1,37 @@
 #!/usr/bin/python3
-
 import ui
 import scipy.misc as smp # Required to export original png
 import numpy as np
 import math # Aids "length" variable
 import multiprocessing as mp # PARALELL PROCESSING
+import time
 
 def getdims(f):
-    """ Processes image with, height, and size.
-    @param f is the file.
-    @return (width, height, filesize(bytes).
-    """
     size = len(f) * .001
-    # width
-    # TODO: make this prettier
     if (size <= 10):
         width = 32
-    elif (10 < size <= 30):
+    elif (size <= 30):
         width = 64
-    elif (30 < size <= 60):
+    elif (size <= 60):
         width = 128
-    elif (60 < size <= 100):
+    elif (size <= 100):
         width = 256
-    elif (100 < size <= 200):
+    elif (size <= 200):
         width = 384
-    elif (200 < size <= 500):
+    elif (size <= 500):
         width = 512
-    elif (500 < size <= 1000):
+    elif (size <= 1000):
         width = 768
     else:
         width = 1024
-
-    # (height, width) 
     return (width, math.ceil(size*1000 // width)+ 1)
-
 
 def makearray(f):
     dimensions = getdims(f)
-    
-    # Creating Image-----------------------------------------------------------
     data = np.array(f)
     data = np.pad(
         data, (0, dimensions[0]-(len(data)%dimensions[0])), 'constant')
     data = np.reshape(data, (-1, dimensions[0]))
-
     return data
 
 def makeimage(name, f):
@@ -51,6 +39,7 @@ def makeimage(name, f):
     smp.imsave(name+"(original).png", img)
 
 def process(files): 
+    start = time.time()
     targets = []
     images = []
 
@@ -66,6 +55,8 @@ def process(files):
                 images.append( makearray(list(f.read())) )
                 makeimage(name, images[-1])
 
+    end = time.time()
+    print("{} files in {} seconds".format(len(files), end-start))
     targets = [name.split('/')[-1][:5] for name in targets]
     return images, targets
 
