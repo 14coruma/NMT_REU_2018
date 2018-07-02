ba7f3dfc560822f25792cdbe8f5554ff022a3a6c
Andrew Corum <14coruma@gmail.com>
2018-07-02 11:09:57 -0600

    * Add ORB feature

--- features.py
+++ features.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 
+import ui
+import progressbar as pb
+
 def features(images):
+    options = ["ORB", "SIFT", "LBP", "Gabor", "Entropy", "LBP and Entropy"]
+    res = ui.prompt("Choose a feature selection algorithm:", options)
+    type = options[int(res)]
+
     data = []
+    for img in pb.progressbar(images): # Process each image
+        if type == "ORB":              # Corner features
+            alg = cv2.ORB_create() 
+            descriptor_size = 32
+            vector_size = 32
+            data.append(describe_keypoints(img, alg, vector_size, descriptor_size))
+        else:
+            print("ERROR: Type " + type + " not found (features.extract_features())\n")
+            return 1
+
     return data
