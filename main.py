dd2dcb968aa33d851833ea113aa91153c2d88bec
Andrew Corum <14coruma@gmail.com>
2018-07-02 09:52:40 -0600

    * Call shell code in main

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 
+import preprocess as pproc
+import features as ft
+import trainer as tr
+
 def main():
-    print("Hello World")
+    images, targets = pproc.process()
+
+    data = ft.features(images)
+
+    tr.train(data, targets)
 
 if __name__ == "__main__":
     main()
