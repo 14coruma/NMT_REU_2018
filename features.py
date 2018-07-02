fe58bea8d048d80c171160f84a9c1e6734446788
Andrew Corum <14coruma@gmail.com>
2018-07-02 11:55:35 -0600

    * Fix ORB feature extraction method. Add SIFT method

--- features.py
+++ features.py
@@ -1,19 +1,50 @@
 #!/usr/bin/python3
+import numpy as np
 
-import ui
-import progressbar as pb
+import cv2
+from skimage import io
+
+import ui # ui.prompt() 
+import progressbar as pb # Display progressbar
+
+def describe_keypoints(img, alg, vector_size, descriptor_size, display=False):
+    """Create description vector for keypoints in an image"""
+    # Finding image keypoints
+    kps = alg.detect(img, None)
+
+    # Get first sorted <vector_size> points.
+    kps = sorted(kps, key=lambda x: x.response)[:vector_size]
+    kps, dsc = alg.compute(img, kps)
+
+    # Fill with zeros if no keypoints are found
+    if len(kps) < vector_size:
+        dsc = np.zeros(shape=(vector_size, descriptor_size))
+
+    # Flatten and normalize descriptors
+    dsc = dsc.flatten()
+    dsc = np.divide(dsc, 256)
+
+    return dsc
 
 def features(images):
     options = ["ORB", "SIFT", "LBP", "Gabor", "Entropy", "LBP and Entropy"]
     res = ui.prompt("Choose a feature selection algorithm:", options)
     type = options[int(res)]
 
+    # Load image for testing. TODO: Remove this line
+    images.append(io.imread("../NMT_REU/data/small/CLEAN_06cofigfs.file.bmp"))
+
     data = []
     for img in pb.progressbar(images): # Process each image
         if type == "ORB":              # Corner features
-            alg = cv2.ORB_create() 
-            descriptor_size = 32
+            alg = cv2.ORB_create()
             vector_size = 32
+            descriptor_size = 32
+            data.append(describe_keypoints(img, alg, vector_size, descriptor_size))
+        elif type == "SIFT":           # Corner features (patented)
+            alg = cv2.xfeatures2d.SIFT_create()
+            vector_size = 32
+            descriptor_size = 32
             data.append(describe_keypoints(img, alg, vector_size, descriptor_size))
         else:
             print("ERROR: Type " + type + " not found (features.extract_features())\n")
