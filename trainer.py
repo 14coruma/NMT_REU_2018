016a9ce893e4dd37a6e614a93adef29e22bf0b81
Andrew Corum <14coruma@gmail.com>
2018-07-03 08:28:40 -0600

    * Add machine learning classification models
    SVM
    RF
    DT
    KNN

--- trainer.py
+++ trainer.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
 
+# ML Classifiers
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+import ui
+
 def train(data, targets):
-    pass
+    options = ["Support Vector Machine", "Random Forest",
+            "Decision Tree Classifier", "KNN"]
+    res = ui.prompt("Choose a ML algorithm:", options)
+    switch = {
+        0: svm.SVC(C=100.),
+        1: RandomForestClassifier(max_depth=2),
+        2: DecisionTreeClassifier(),
+        3: KNeighborsClassifier()
+    }
+    clf = switch.get(int(res))
