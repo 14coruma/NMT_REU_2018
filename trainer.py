f75d52b64d06ef0566a106f05762e306041624a7
Andrew Corum <14coruma@gmail.com>
2018-07-16 15:57:34 -0600

    * Move classifier selection code out of cross_val function (Will make code more DRY later)

--- trainer.py
+++ trainer.py
@@ -12,7 +12,25 @@ from sklearn.model_selection import cross_validate
 
 import ui
     
-def model_evaluation(data, targets):
+def model_evaluation(data, targets, clf):
+    # Cross validate and calculate scores
+    scoring = ["accuracy", "precision", "recall", "f1"] # Choose scoring methods
+    scores = cross_validate(clf, data, targets, scoring=scoring, cv=5)
+    print("Scores calculated from 5-fold cross validation:")
+    print("Accuracy:  {},\t{}".format(round(np.mean(scores["test_accuracy"]),  4), scores["test_accuracy"]))
+    print("Precision: {},\t{}".format(round(np.mean(scores["test_precision"]), 4), scores["test_precision"]))
+    print("Recall:    {},\t{}".format(round(np.mean(scores["test_recall"]),    4), scores["test_recall"]))
+    print("F1:        {},\t{}".format(round(np.mean(scores["test_f1"]),        4), scores["test_f1"]))
+
+def train(data, targets):
+    targets = [val == "INFEC" for val in targets] # Set INFEC as positive val
+   
+    # Choose training mode
+    options = ["Cross validation", "Build and test model"]
+    res = ui.prompt(options=options)
+    mode = options[int(res)]
+
+    # Choose ML algorithm
     options = ["Support Vector Machine", "Random Forest",
             "Decision Tree Classifier", "KNN"]
     res = ui.prompt("Choose a ML algorithm:", options)
@@ -24,22 +42,7 @@ def model_evaluation(data, targets):
     }
     clf = switch.get(int(res))
 
-    # Cross validate and calculate scores
-    scoring = ["accuracy", "precision", "recall", "f1"] # Choose scoring methods
-    targets = [val == "INFEC" for val in targets] # Set INFEC as positive val
-    scores = cross_validate(clf, data, targets, scoring=scoring, cv=5)
-    print("Scores calculated from 5-fold cross validation:")
-    print("Accuracy:  {},\t{}".format(round(np.mean(scores["test_accuracy"]),  4), scores["test_accuracy"]))
-    print("Precision: {},\t{}".format(round(np.mean(scores["test_precision"]), 4), scores["test_precision"]))
-    print("Recall:    {},\t{}".format(round(np.mean(scores["test_recall"]),    4), scores["test_recall"]))
-    print("F1:        {},\t{}".format(round(np.mean(scores["test_f1"]),        4), scores["test_f1"]))
-
-def train(data, targets):
-    options = ["Cross validation", "Build and test model"]
-    res = ui.prompt(options=options)
-    mode = options[int(res)]
-
     if mode == "Cross validation":
-        model_evaluation(data, targets)
+        model_evaluation(data, targets, clf)
     elif mode == "Build and test model":
-        print("HERE");
+        print("HERE") 
