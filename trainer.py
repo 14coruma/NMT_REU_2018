* Fix accuracy and precision values. Note, confusion matrix is backwards now

trainer.py
@@ -38,11 +38,27 @@ def save_filenames(y_true, y_pred, filenames):
     np.savetxt("results/{}_false_neg.txt".format(prefix), fn, delimiter=",", fmt="%s")
     return 0
 
+def true_neg(y_true, y_pred, args=None):
+    """Count number of true negatives"""
+    fp_count = 0
+    for i in range(len(y_true)):
+        if not y_true[i] and not y_pred[i]:
+            fp_count += 1
+    return fp_count
+
+def true_pos(y_true, y_pred, args=None):
+    """Count number of true positives"""
+    fp_count = 0
+    for i in range(len(y_true)):
+        if y_true[i] and y_pred[i]:
+            fp_count += 1
+    return fp_count
+
 def false_pos(y_true, y_pred, args=None):
     """Count number of false positives"""
     fp_count = 0
     for i in range(len(y_true)):
-        if y_true[i] and not y_pred[i]:
+        if not y_true[i] and y_pred[i]:
             fp_count += 1
     return fp_count
 
@@ -50,7 +66,7 @@ def false_neg(y_true, y_pred, args=None):
     """Count number of false negatives"""
     fn_count = 0
     for i in range(len(y_true)):
-        if not y_true[i] and y_pred[i]:
+        if y_true[i] and not y_pred[i]:
             fn_count += 1
     return fn_count
 
@@ -58,6 +74,8 @@ def model_evaluation(data, targets, clf):
     # Cross validate and calculate scores
     f_pos = skm.make_scorer(false_pos)
     f_neg = skm.make_scorer(false_neg)
+    t_pos = skm.make_scorer(true_pos)
+    t_neg = skm.make_scorer(true_neg)
     scoring = {
             "accuracy": "accuracy",
             "precision": "precision",
@@ -65,6 +83,8 @@ def model_evaluation(data, targets, clf):
             "f1": "f1",
             "f_pos": f_pos,
             "f_neg": f_neg,
+            "t_pos": t_pos,
+            "t_neg": t_neg,
     }
     scores = cross_validate(clf, data, targets, scoring=scoring, cv=10)
     print("Scores calculated from 10-fold cross validation:")
@@ -74,9 +94,11 @@ def model_evaluation(data, targets, clf):
     print("F1:        {},\t{}".format(round(np.mean(scores["test_f1"]),        4), scores["test_f1"]))
     print("False Pos: {},\t{}".format(round(np.mean(scores["test_f_pos"]),     4), scores["test_f_pos"]))
     print("False Neg: {},\t{}".format(round(np.mean(scores["test_f_neg"]),     4), scores["test_f_neg"]))
+    print("True Pos:  {},\t{}".format(round(np.mean(scores["test_t_pos"]),     4), scores["test_t_pos"]))
+    print("True Neg:  {},\t{}".format(round(np.mean(scores["test_t_neg"]),     4), scores["test_t_neg"]))
 
 def train(data, targets, filenames):
-    targets = [val == "CLEAN" for val in targets] # Set INFEC as positive val
+    targets = [val == "INFEC" for val in targets] # Set INFEC as positive val
    
     # Choose training mode
     options = ["Cross validation", "Build and test model"]
@@ -114,7 +136,7 @@ def train(data, targets, filenames):
         print("************************************")
 
         pageNames, y_true, filenames = pproc.process(dirname)    
-        y_true = [val == "CLEAN" for val in y_true] # Set INFEC as positive val
+        y_true = [val == "INFEC" for val in y_true] # Set INFEC as positive val
         test_data = ft.features(pageNames)
    
         y_pred = clf.predict(test_data)
@@ -128,6 +150,6 @@ def train(data, targets, filenames):
         f1 = skm.f1_score(y_true, y_pred, average=None)
         print("\n{}".format(conf_matrix))
         print("Accuracy:  {}".format(accuracy))
-        print("Precision: {}".format(precision[0]))
-        print("Recall:    {}".format(recall[0]))
-        print("F1:        {}".format(f1[0]))
+        print("Precision: {}".format(precision[1]))
+        print("Recall:    {}".format(recall[1]))
+        print("F1:        {}".format(f1[1]))